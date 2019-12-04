56573d521f93364dbad9f78cd33bf143e2b5b8fb
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-04 19:04:28 +0200

    task-1

---
+++ task-1.js
@@ -0,0 +1,7 @@
+const name = "«Генератор защитного поля»";
+const price = 1000;
+const message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(message);
+const newPrice = 2000;
+const newMessage = `Выбран ${name}, цена за штуку ${newPrice} кредитов`;
+console.log(newMessage);
