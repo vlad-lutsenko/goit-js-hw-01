3a8ae6d77acd8c91ba82b0643c30a4d514b69f1e
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-04 19:18:48 +0200

    task-2

--- task-1.js
+++ task-1.js
@@ -1,3 +1,4 @@
+"use strict";
 const name = "«Генератор защитного поля»";
 const price = 1000;
 const message = `Выбран ${name}, цена за штуку ${price} кредитов`;
---
+++ task-2.js
@@ -0,0 +1,10 @@
+"use strict";
+const total = 100;
+const ordered = 130;
+if (total < ordered) {
+  const message = "На складе недостаточно твоаров!";
+  console.log(message);
+} else {
+  const message = "Заказ оформлен, с вами свяжется менеджер";
+  console.log(message);
+}
