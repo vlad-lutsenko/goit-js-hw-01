385c2904acec6266ae5ffcfe967419d74d8b0401
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-04 21:22:37 +0200

    task-5

---
+++ task-5.js
@@ -0,0 +1,34 @@
+"use strict";
+const order = prompt("введите страну доставки");
+
+const priceChina = 100;
+const priceChile = 250;
+const priceAustralia = 170;
+const priceIndia = 80;
+const priceJamaica = 120;
+
+if (order === null) {
+  alert("отменено пользователем");
+}
+
+switch (order.toLowerCase()) {
+  case "китай":
+    alert(`Доставка в ${order} будет стоить ${priceChina} кредитов`);
+    break;
+  case "чили":
+    alert(`Доставка в ${order} будет стоить ${priceChile} кредитов`);
+    break;
+  case "австралия":
+    alert(`Доставка в ${order} будет стоить ${priceAustralia} кредитов`);
+    break;
+  case "индия":
+    alert(`Доставка в ${order} будет стоить ${priceIndia} кредитов`);
+    break;
+  case "ямайка":
+    alert(`Доставка в ${order} будет стоить ${priceJamaica} кредитов`);
+    break;
+
+  default:
+    alert("Извините, но в вашей стране доставка не доступна");
+    break;
+}
