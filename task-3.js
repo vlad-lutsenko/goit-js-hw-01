17d3b09f23e805965dadd83d40f0e422fd1e2a75
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-06 01:19:01 +0200

    no more comments

--- task-3.js
+++ task-3.js
@@ -4,12 +4,9 @@ let message;
 let password = prompt("введите пароль");
 if (password === null) {
   message = "Отменено пользователем!";
-  // console.log(message);
 } else if (password === ADMIN_PASSWORD) {
   message = "Добро пожаловать!";
-  // console.log(message);
 } else {
   message = "Доступ запрещен, неверный пароль!";
-  // console.log(message);
 }
 alert(message);
