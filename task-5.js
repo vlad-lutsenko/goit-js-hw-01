75e775921e22e99dfb00b6ca3d7b51165566b0ae
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-10 20:02:01 +0200

    order.toLowerCase hidden in else

--- task-5.js
+++ task-5.js
@@ -10,26 +10,26 @@ const priceJamaica = 120;
 
 if (order === null) {
   alert("отменено пользователем");
-}
+} else {
+  switch (order.toLowerCase()) {
+    case "китай":
+      alert(`Доставка в ${order} будет стоить ${priceChina} кредитов`);
+      break;
+    case "чили":
+      alert(`Доставка в ${order} будет стоить ${priceChile} кредитов`);
+      break;
+    case "австралия":
+      alert(`Доставка в ${order} будет стоить ${priceAustralia} кредитов`);
+      break;
+    case "индия":
+      alert(`Доставка в ${order} будет стоить ${priceIndia} кредитов`);
+      break;
+    case "ямайка":
+      alert(`Доставка в ${order} будет стоить ${priceJamaica} кредитов`);
+      break;
 
-switch (order.toLowerCase()) {
-  case "китай":
-    alert(`Доставка в ${order} будет стоить ${priceChina} кредитов`);
-    break;
-  case "чили":
-    alert(`Доставка в ${order} будет стоить ${priceChile} кредитов`);
-    break;
-  case "австралия":
-    alert(`Доставка в ${order} будет стоить ${priceAustralia} кредитов`);
-    break;
-  case "индия":
-    alert(`Доставка в ${order} будет стоить ${priceIndia} кредитов`);
-    break;
-  case "ямайка":
-    alert(`Доставка в ${order} будет стоить ${priceJamaica} кредитов`);
-    break;
-
-  default:
-    alert("Извините, но в вашей стране доставка не доступна");
-    break;
+    default:
+      alert("Извините, но в вашей стране доставка не доступна");
+      break;
+  }
 }
